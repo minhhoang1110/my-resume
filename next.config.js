1604620b7eb9680dbc9119565cd4ba4b56ddaed5
Truong Minh Hoang <minhhoang111099@gmail.com>
2023-03-28 14:35:23 +0700

    update service worker

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withOffline = require("next-offline");
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = withOffline(nextConfig);
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { DEFAULT_BG_COLOR } from "@/configs/constants";
 import store from "@/store";
 import { Provider } from "react-redux";
+import { useEffect } from "react";
 const theme = extendTheme({
   styles: {
     global: () => ({
@@ -14,6 +15,23 @@ const theme = extendTheme({
   },
 });
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/sw.js").then(
+          (registration) => {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          (err) => {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, []);
   return (
     <Provider store={store}>
       <ChakraProvider theme={theme}>
